3a76a5ab6c7b0642330f1540132636e86479c3fb
Emilia Pierchała <emilapc2@gmail.com>
2018-08-13 18:02:10 +0200

    Added tile highlighting script

---
+++ Assets/Scripts/HighlightTarget.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightTarget : MonoBehaviour
+{
+	Highlightable currentHighlightable;
+
+	void Update()
+	{
+
+
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			Highlightable highlightable = hit.collider.gameObject.GetComponent<Highlightable>();
+
+			if (highlightable != null)
+			{
+
+				if (currentHighlightable != highlightable)
+				{
+					if (currentHighlightable != null)
+					{
+						currentHighlightable.Unhighlight();
+					}
+
+					currentHighlightable = highlightable;
+					Debug.Log("Kupka");
+					highlightable.Highlight();
+				}
+
+			}
+			else
+			{
+				currentHighlightable.Unhighlight();
+				currentHighlightable = null;
+			}
+
+		}
+		else
+		{
+			currentHighlightable.Unhighlight();
+		}
+
+
+
+
+	}
+}
---
+++ Assets/Scripts/Highlightable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlightable : MonoBehaviour
+{
+	public Color highlightColor;
+	Color baseColor;
+	Color targetColor;
+	Renderer renderer;
+	public float colorTransitionSpeed = 10;
+
+	private void Awake()
+	{
+		renderer = GetComponentInChildren<Renderer>();
+		baseColor = renderer.materials[1].color;
+		targetColor = baseColor;
+	}
+
+	private void Update()
+	{
+		if (renderer.materials[1].color.Equals(targetColor) == false)
+		{
+			renderer.materials[1].color = Color.Lerp(renderer.materials[1].color, targetColor, colorTransitionSpeed * Time.deltaTime);
+		}
+	}
+
+	public void Highlight()
+	{
+
+		targetColor = highlightColor;
+
+	}
+
+	public void Unhighlight()
+	{
+		targetColor = baseColor;
+	}
+
+}
